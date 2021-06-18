d8ee6e799883462bfc090769ca742c33af716918
Ingunn <ingunnfu@gmail.com>
2021-06-18 13:19:34 +0200

    ny mappe

--- wallet/wallet-app/App.js
+++ wallet/wallet-app/App.js
@@ -16,8 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#2FFF12',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
