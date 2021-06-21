07c4620d3be161a2465768b8f280f47c9539d64d
Inger Yri <yriinger@gmail.com>
2021-06-21 15:22:11 +0200

    Enkel meny løsning

--- wallet/wallet-app/App.js
+++ wallet/wallet-app/App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import MenuButtons from './components/menu';
 
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style>Lommeboka!</Text>
+      <Text>Lommeboka!</Text>
       <StatusBar style="auto" />
+      <MenuButtons/>
     </View>
   );
 }
@@ -16,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2FFF12',
+    backgroundColor: '#87D7BF',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ wallet/wallet-app/components/menu.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, StyleSheet, Button, Alert,TouchableOpacity} from "react-native";
+
+const Separator = () => (
+    <View style={styleSheets.separator} />
+);
+
+export default function MenuButtons() {
+    
+    const click = () => alert('Pressed')
+
+    return(
+    <View style={styles.container}>
+       
+
+    <TouchableOpacity
+            style={styles.button}
+            onPress={click}>
+            <Text>Etterspørsler</Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity
+            style={styles.buttonTo}
+            onPress={click}>
+            <Text>Forespørsler</Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity
+            style={styles.buttonTre}
+            onPress={click}>
+            <Text>Oversikt</Text>
+
+      </TouchableOpacity>
+
+    </View>
+        
+    );}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        paddingHorizontal: 10,
+        
+    },
+    button: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: 'flex-start',
+        backgroundColor: "#CDE6EA",
+        padding: 10,
+        width:100,
+        hight:100,
+        
+    },
+    buttonTo: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#CDE6EA",
+        padding: 10,
+        width:100,
+        hight:100
+
+    },
+    buttonTre: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: 'flex-end',
+        backgroundColor: "#CDE6EA",
+        padding: 10,
+        width:100,
+        hight:100
+    }
+
+
+    
+    });
