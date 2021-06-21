573567d0180e2cb867af8d003b02282a31e524c0
Gunvor <gunvor_huso@hotmail.com>
2021-06-21 14:32:42 +0200

    styling

--- verifier/verifier-app/src/Pages/NotVerified.jsx
+++ verifier/verifier-app/src/Pages/NotVerified.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function NotVerified() {
 
     return (
         <div className="NotVerified">
             <p>You were not verified!</p>
+            <Link className="btn" to={'/'}>Back</Link>
         </div>
     );
 }
