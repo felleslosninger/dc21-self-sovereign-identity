3e5a17bb520bee4dea62de4d5f3c01aa4bbcd55a
Eirik Schøien <eschoien@gmail.com>
2021-06-24 12:44:12 +0200

    #14 Refactor and improve http request

--- wallet/wallet-app/components/frames/ActivityFrame.jsx
+++ wallet/wallet-app/components/frames/ActivityFrame.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
-import TheRequests from '../../requestsex';
 import ConsentButton from '../ConsentButton';
 import Menu from '../Menu';
 
@@ -8,7 +7,6 @@ export default function ActivityFrame() {
   return (
     <SafeAreaView>
       <ConsentButton></ConsentButton>
-      <TheRequests></TheRequests>
       <Menu></Menu>
     </SafeAreaView>
   );
---
+++ wallet/wallet-app/utils/httpRequests.js
@@ -0,0 +1,13 @@
+export async function httpGetCredential(statement) {
+  let url = 'http://localhost:8083/api/getCredential/';
+
+  /*const requestOptions = {
+            method: 'GET',
+            headers: {'Conent-Type': 'text/html',
+                    "access-control-allow-origin" : "*"},
+    };*/
+  let response = await fetch(url + statement); //, requestOptions);
+  let payload = await response.text();
+  console.log(payload);
+  return payload;
+}
