c809f182d3d0ef3624e6c5c85ef2c755158f6905
Juni <juni.bugge01@gmail.com>
2021-06-28 14:24:57 +0200

    added signature

--- wallet/wallet-app/utils/signnew.js
+++ wallet/wallet-app/utils/signnew.js
@@ -1,7 +1,27 @@
-var rsaKeypair = KEYUTIL.generateKeypair("RSA", 1024);
+import { KEYUTIL } from 'jsrsasign';
+import { KJUR } from 'jsrsasign';
 
-// RSA signature generation
-var sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
-sig.init(prvKeyPEM);
-sig.updateString('aaa');
-var hSigVal = sig.sign();
+export const sign = async () => {
+  var rsaKeypair = KEYUTIL.generateKeypair('RSA', 2048);
+
+  // RSA signature generation
+  var sig = new KJUR.crypto.Signature({ alg: 'SHA256withRSA' });
+  console.log(sig);
+
+  var publicKey = rsaKeypair.pubKeyObj;
+  console.log(publicKey);
+  var privateKey = rsaKeypair.prvKeyObj;
+  console.log(privateKey);
+
+  sig.init(privateKey);
+  sig.updateString('hallo');
+  var signature = sig.sign();
+  console.log(signature);
+
+  // DSA signature validation
+  var sig2 = new KJUR.crypto.Signature({ alg: 'SHA256withRSA' });
+  sig2.init(publicKey);
+  sig2.updateString('hallo');
+  var isValid = sig2.verify(signature);
+  console.log(isValid);
+};
