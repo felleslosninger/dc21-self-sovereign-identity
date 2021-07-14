30990dd826b6fa80ad32a2adf25a7d81618dcc4f
h570611 <jonasweert@hotmail.com>
2021-07-14 16:04:48 +0200

    onboarding with qr-code-scanner

--- wallet/wallet-app/components/frames/Onboarding.jsx
+++ wallet/wallet-app/components/frames/Onboarding.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-/**
- * Here we are going to add the onboarding
- * @returns 
- */
-export default function Onboarding() {
+export default function App() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Opprettelse av bruker</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={StyleSheet.absoluteFillObject}
+            />
+            {scanned && <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />}
+        </View>
     );
 }
 
