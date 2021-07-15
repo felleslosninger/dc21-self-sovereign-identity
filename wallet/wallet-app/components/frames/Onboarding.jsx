3377f4a826d74fe78ec6875602d9c84dded2ebf1
h570611 <jonasweert@hotmail.com>
2021-07-15 10:25:16 +0200

    QR-scanning OK. With button

--- wallet/wallet-app/components/frames/Onboarding.jsx
+++ wallet/wallet-app/components/frames/Onboarding.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import jwtDecode from 'jwt-decode';
 import { useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 import { addCredential } from '../../redux/CredentialSlice';
 
 export default function App() {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [verified, setVerified] = useState(false);
     const dispatch = useDispatch();
+    const navigation = useNavigation();
 
     useEffect(() => {
         (async () => {
@@ -22,12 +25,21 @@ export default function App() {
         setScanned(true);
         // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
 
-        const decode = jwtDecode(data);
+        const baseId = jwtDecode(data);
+        const types = baseId.vc.type;
+
         // const retrievedCredential = { ...decode, data, type: 'Grunn-id' };
-        const retrievedCredential = { ...decode };
+        const retrievedCredential = { ...baseId, type: 'Grunn-id' };
+
+        // If checkVerified => setVerified(true)
+        if (types.includes('BaseCredential')) {
+            setVerified(true);
+        }
+
         dispatch(addCredential(retrievedCredential));
         await AsyncStorage.setItem('Grunn-id', JSON.stringify(retrievedCredential));
-        alert(JSON.stringify(retrievedCredential));
+
+        // alert(JSON.stringify(retrievedCredential));
         // console.log(retrievedCredential);
     };
 
@@ -40,19 +52,43 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-            <BarCodeScanner
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-                style={StyleSheet.absoluteFillObject}
-            />
-            {scanned && <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />}
+            <View style={styles.camera}>
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+                {scanned && <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />}
+            </View>
+            {scanned && verified ? (
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Adgangskontroll')}>
+                    <Text style={styles.text}>Lagre grunnidentitet</Text>
+                </TouchableOpacity>
+            ) : null}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    camera: {
+        flex: 1,
+        marginTop: 80,
+        marginBottom: 80,
+        alignItems: 'center',
+    },
     container: {
         flex: 1,
-        marginTop: 175,
+    },
+    button: {
+        borderRadius: 4,
+        backgroundColor: '#3aa797',
+        padding: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        width: '75%',
         alignItems: 'center',
+        alignSelf: 'center',
+    },
+    text: {
+        fontSize: 25,
     },
 });
