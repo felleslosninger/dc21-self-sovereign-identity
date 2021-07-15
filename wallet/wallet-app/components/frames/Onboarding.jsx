101a19478d0a780dc1d007fb15369e30d67057a5
h570611 <jonasweert@hotmail.com>
2021-07-15 09:23:52 +0200

    Read QR-code OK.

--- wallet/wallet-app/components/frames/Onboarding.jsx
+++ wallet/wallet-app/components/frames/Onboarding.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import jwtDecode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { addCredential } from '../../redux/CredentialSlice';
 
 export default function App() {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         (async () => {
@@ -13,9 +18,17 @@ export default function App() {
         })();
     }, []);
 
-    const handleBarCodeScanned = ({ type, data }) => {
+    const handleBarCodeScanned = async ({ type, data }) => {
         setScanned(true);
-        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+        const decode = jwtDecode(data);
+        // const retrievedCredential = { ...decode, data, type: 'Grunn-id' };
+        const retrievedCredential = { ...decode };
+        dispatch(addCredential(retrievedCredential));
+        await AsyncStorage.setItem('Grunn-id', JSON.stringify(retrievedCredential));
+        alert(JSON.stringify(retrievedCredential));
+        // console.log(retrievedCredential);
     };
 
     if (hasPermission === null) {
