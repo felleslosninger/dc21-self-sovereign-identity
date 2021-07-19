4e4dd167d0745cf4c91ab15f40e656b1a7b095d6
Martin <martin.burgos99@gmail.com>
2021-07-19 14:09:51 +0200

    Added default public and private keys. And handler files for them both

--- issuer/src/main/java/com/digdir/issuer/storage/PrivateKeysFileHandler.java
+++ issuer/src/main/java/com/digdir/issuer/storage/PrivateKeysFileHandler.java
@@ -1,44 +1,37 @@
 package com.digdir.issuer.storage;
 
-
+import com.digdir.issuer.util.KeyGenerator;
 import com.google.gson.Gson;
-
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import org.springframework.stereotype.Repository;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
+import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Objects;
 
-/**
- * Class as Repository for handle operations with files.
- */
-@Repository
-public class FileHandler {
 
-    private String path = "issuer/src/main/resources/PublicKeyFile.json";
+public class PrivateKeysFileHandler {
+    private String path = "issuer/src/main/resources/PrivateKeys.json";
     private Writer file;
 
     /**
-     * Method for adding a public key to VDR(PublicKeyFile.json).
+     * Method for adding a private key to localStorage.
      * Retrives existing map of PK's in file and appends PK.
      *
      * @param id id for identifying a public key to it's owner in VDR.
      * @param pk Public key to be saved to VDR.
      */
-    public void addPublicKey(String id, PublicKey pk){
-             HashMap<String, PublicKey> map = loadFromFile();
-            map.put(id, pk);
-            saveToFile(map);
+    public void addPrivateKey(String id, PrivateKey pk){
+        HashMap<String, PrivateKey> map = loadFromFile();
+        map.put(id, pk);
+        saveToFile(map);
 
 
     }
@@ -48,7 +41,7 @@ public class FileHandler {
      * @param id id for public key to retrive
      * @return Public Key with id as specified, if existing.
      */
-    public PublicKey getPublicKey(String id){
+    public PrivateKey getPublicKey(String id){
         if (!Objects.requireNonNull(loadFromFile()).containsKey(id)) {
             throw new IllegalArgumentException("No such id");
         }
@@ -58,13 +51,13 @@ public class FileHandler {
 
     /**
      * Method for saving HashMap of id and PublicKey to file.
-     * @param publicKeyMap HashMap to save.
+     * @param privateKeyHashMap HashMap to save.
      */
-    private void saveToFile(HashMap<String, PublicKey> publicKeyMap) {
+    private void saveToFile(HashMap<String, PrivateKey> privateKeyHashMap) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         HashMap<String, String> map = new HashMap<>();
-        publicKeyMap.forEach((key, value) -> map.put(key, Base64.getEncoder().encodeToString(value.getEncoded())));
+        privateKeyHashMap.forEach((key, value) -> map.put(key, Base64.getEncoder().encodeToString(value.getEncoded())));
         //publicKeyMap.forEach((key, value) -> map.put(key, value.getEncoded()));
 
         String javaObjectString = gson.toJson(map); // converts to json
@@ -93,22 +86,22 @@ public class FileHandler {
      * Loads existing HashMap of id and PublicKey from PublicKeyFile.json
      * @return HashMap of id and PublicKey
      */
-    private HashMap<String, PublicKey> loadFromFile() {
+    private HashMap<String, PrivateKey> loadFromFile() {
         try {
             InputStream inputStream = new FileInputStream(path);
             Reader fileReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
             HashMap<String, String> mapFromFile = new Gson().fromJson(fileReader, new TypeToken<HashMap<String, String>>() {
             }.getType());
-            HashMap<String, PublicKey> publicKeyMap = new HashMap<>();
+            HashMap<String, PrivateKey> privateKeyMap = new HashMap<>();
             mapFromFile.forEach((key, value) -> {
                 try {
                     //publicKeyMap.put(key, KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(value.getBytes(StandardCharsets.UTF_8))));
-                    publicKeyMap.put(key, KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(value))));
+                    privateKeyMap.put(key, KeyFactory.getInstance("RSA").generatePrivate(new X509EncodedKeySpec((Base64.getDecoder().decode(value)))));
                 } catch (InvalidKeySpecException | NoSuchAlgorithmException invalidKeySpecException) {
                     System.out.println("Problem in Filehandler. Cant load from file. ");
                 }
             });
-            return publicKeyMap;
+            return privateKeyMap;
 
         } catch (Exception e) {
             throw new NullPointerException("There was an error loading from file.");
@@ -125,17 +118,24 @@ public class FileHandler {
         if(!loadFromFile().containsKey(id)) {
             throw new IllegalArgumentException("no such id, cannot remove");
         }
-        HashMap<String, PublicKey> map = loadFromFile();
+        HashMap<String, PrivateKey> map = loadFromFile();
         map.remove(id);
         saveToFile(map);
 
     }
 
-
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = new KeyGenerator();
+        keyGenerator.getPrivateKey();
+        PrivateKeysFileHandler privateKeysFileHandler = new PrivateKeysFileHandler();
+        FileHandler fileHandler = new FileHandler();
+        privateKeysFileHandler.addPrivateKey("BasePrivate",keyGenerator.getPrivateKey());
+        fileHandler.addPublicKey("BasePublic", keyGenerator.getPublicKey());
+    }
 
     // Used in test: -----------------------------------------------------------------------
     public String getPublicKeyAsString(String id){
-        PublicKey pk = getPublicKey(id);
+        PrivateKey pk = getPublicKey(id);
         byte[] jsonPk = pk.getEncoded();
         Gson gson = new Gson();
         return gson.toJson(jsonPk);
