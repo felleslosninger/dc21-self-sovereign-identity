3a6f278f5990b770df8b3feb2cbe0ff787a05660
Juni <juni.bugge01@gmail.com>
2021-07-21 11:05:13 +0200

    Fixed skip onboarding on web

--- wallet/wallet-app/components/frames/StartPage.jsx
+++ wallet/wallet-app/components/frames/StartPage.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { TouchableOpacity, SafeAreaView, Text, StyleSheet, Platform } from 'react-native';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { generateKeys } from '../../utils/sign';
 
 /**
  * The intro page to the app, choose between log in or create a user
@@ -11,43 +10,27 @@ import { generateKeys } from '../../utils/sign';
  */
 export default function StartPage() {
     const navigation = useNavigation();
-    const [hasBaseId, setBaseID] = useState(false);
     const isFocused = useIsFocused();
+    const [onboarded, setOnboarded] = useState(false);
 
-    const checkBaseId = async () => {
-        const value = await AsyncStorage.getItem('baseId');
-        if (value !== null) {
-            setBaseID(true);
+    const checkOnboarded = async () => {
+        const baseId = await AsyncStorage.getItem('baseId');
+        const walletId = await AsyncStorage.getItem('walletID');
+        const privateKey = await AsyncStorage.getItem('privateKey');
+        const pin = await AsyncStorage.getItem('pin');
+
+        if (baseId && walletId && privateKey && pin) {
+            setOnboarded(true);
+        } else {
+            setOnboarded(false);
         }
     };
 
-    // ----------
-    // Skip onboarding when launcing on web
-
-    const exampleBaseVc =
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIwODA4OTQwODA4NCIsImlzcyI6IkdydW5uSUQtcG9ydGFsZW4ubm85NDAxNWViYi0yMzBjLTQyMWQtOWMwZC1mNDE2NjEzNzQwNzgiLCJleHAiOjE2Mjc5MDU1MzMsImlhdCI6MTYyNjY5NTkzMywidmMiOnsiY3JlZGVudGlhbFN1YmplY3QiOnsiYmFzZWlkIjp7Im5hbWUiOiJCYXNlSUQiLCJ0eXBlIjoiQmFzZUlEIn19LCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiQmFzZUNyZWRlbnRpYWwiXSwiQGNvbnRleHQiOlsiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiXX0sImp0aSI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4My9jcmVkZW50aWFscy8xIn0.hNGmIyFzRIRm0dM0QX94umAt9egcN0mZ7zwVifAxsfMe7n4KQG5mtTRF7eDSecUicov24lskL09LhEDHGNY9EThjVOfh3cKoZd5g78qdARgpWnaXeFRDZ8Nx7mqUeKq1O4yiMcgc81pQJrH09lFfp-5PIj4KYSDLJxNFIuAOSonNpaiIHEJrwpqziWZhci15MBg7Zyu7xgD4-NWw6uc6lwDavCQ_CGB8tO2j-rMZuxHlwvjxgBVyXKTayPnPAUyiBE6xERt4NH9zTCMhSNua4nPlq4FqwFzbUEYpFbkw-UvJGSb7o0FhJqt0RP0Zdrv5Hs95tC0KP0-trNtViO7NAg';
-
-    const skipOnboarding = async () => {
-        await AsyncStorage.setItem('pin', '1111');
-        await AsyncStorage.setItem('baseId', exampleBaseVc);
-        await generateKeys();
-    };
-
-    const checkPlatform = () => {
-        if (Platform.OS === 'web') {
-            skipOnboarding();
-        }
-    };
-
-    isFocused ? checkPlatform() : null;
-
-    // ----------
-
-    checkBaseId();
+    isFocused ? checkOnboarded() : null;
 
     return (
         <SafeAreaView style={styles.container}>
-            {hasBaseId ? (
+            {onboarded ? (
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Adgangskontroll')}>
                     <Text style={styles.text}>Logg inn i lommeboka</Text>
                 </TouchableOpacity>
