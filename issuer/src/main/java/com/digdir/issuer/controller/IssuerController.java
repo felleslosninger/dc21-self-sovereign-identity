3359dda8d69a12386b1ee1f64e07dfe2c7e93e46
Martin <martin.burgos99@gmail.com>
2021-07-29 13:54:58 +0200

    Added issuer to endpoint get vc endpoint

--- issuer/src/main/java/com/digdir/issuer/controller/IssuerController.java
+++ issuer/src/main/java/com/digdir/issuer/controller/IssuerController.java
@@ -1,6 +1,7 @@
 package com.digdir.issuer.controller;
 
 import com.digdir.issuer.service.VcService;
+import com.digdir.issuer.storage.IssuerTypesHandler;
 import com.digdir.issuer.storage.JwtTypeHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,17 +16,26 @@ import java.util.Collection;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class IssuerController {
     private final VcService vcService;
+    private final IssuerTypesHandler issuerTypesHandler;
 
     /**
-     * Route that handles issuance of certain VC, it requiers a valid baseVC to be input
+     * Route that handles issuance of certain VC, it requires a valid baseVC to be input
      *
      * @param type   Type of credential, that is checked in the "database" if the type exists
      * @param baseVC The VC gotten from the baseId issuer by logging in through id-porten.
-     * @return A VC in the format of a JWT, this is signed with issuer public key and can be verified by verifier by gettting that key from the VDR
+     * @param issuer The issuer, if valid issuer exists.
+     * @return A VC in the format of a JWT, this is signed with issuer public key and can be verified by verifier by getting that key from the VDR
      */
     @GetMapping("/api/getVC")
-    public String getVC(@RequestParam(value = "type", defaultValue = "defaultType") String type, @RequestParam(value = "baseVC", defaultValue = "defaultVC") String baseVC) {
-        return vcService.getVC(type, baseVC);
+    public String getVCOverload(@RequestParam(value = "type", defaultValue = "defaultType") String type, @RequestParam(value = "baseVC", defaultValue = "defaultVC") String baseVC, @RequestParam(value = "issuer", defaultValue = "defaultIss") String issuer) {
+        if (issuer.equals("defaultIss")){
+            return "No issuer input";
+        }
+        if (issuerTypesHandler.getTypesWithIssuer(issuer) == null) {
+            return "No issuer exists for this VC";
+        }
+        //TODO: Add type check
+        return vcService.getVC(type, baseVC, issuer);
     }
 
     /**
